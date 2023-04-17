cfcce012230c2fb2feb4cf6efe98f58cb795cb28
Evgeny <bril424@mail.ru>
2023-04-17 14:16:56 +0200

    Rare primitive types symbol and bigint

--- TypeNullUndefined/index.ts
+++ TypeNullUndefined/index.ts
@@ -1,23 +1,38 @@
-const test: null = null;
-const test2: any = null;
+// const test: null = null;
+// const test2: any = null;
 // const text3: string = null;
 // const test4: number = null;
 
-const test5: undefined = undefined;
-const text6:any = undefined;
+// const test5: undefined = undefined;
+// const text6:any = undefined;
 // const test7: string =undefined;
 
-let smth;
+// let smth;
 
-const getRndData = () => {
-    if ( Math.random() < 0.5) {
-        return null;
-    } else {
-        return " Some data ";
-    }
-}
+// const getRndData = () => {
+//     if ( Math.random() < 0.5) {
+//         return null;
+//     } else {
+//         return " Some data ";
+//     }
+// }
 
-const data = getRndData()
-const trimmedData = data ? data.trim() : null;
+// const data = getRndData()
+// const trimmedData = data ? data.trim() : null;
 
-console.log(trimmedData)
+// console.log(trimmedData)
+
+// Rare primitive types symbol and bigint
+
+let id: symbol = Symbol("id");
+
+const data1 = { 
+    [id]: 1,
+};
+
+console.log(data1[id]);
+
+const num1: bigint = 1n;
+const num2: bigint = 2n;
+
+console.log(num1 + num2);
