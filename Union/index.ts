9ea38e8c5acc6659da641dd9874e37bff8bb46b3
Evgeny <bril424@mail.ru>
2023-04-20 14:36:56 +0200

    Union added

---
+++ Union/index.ts
@@ -0,0 +1,10 @@
+const message: string | number = 5;
+const messages: string[] | number[] = ['a', 'b'];
+
+
+const printMsg = (msg: string | number): void => {
+    console.log(msg);
+} 
+
+printMsg(4)
+printMsg('hallo')
