4e14ac09fd8f5bd373ee966810e0b92d204bfb22
Evgeny <bril424@mail.ru>
2023-04-24 13:37:10 +0200

    Literal types added

---
+++ LiteralTypes/index.ts
@@ -0,0 +1,34 @@
+let msg: "Hello" = "Hello"
+
+msg = "Hello"
+
+const port3000: number = 3000;
+const port3001: number = 3001
+
+const startServer = (protocol: "http" | "https", port: 3000 | 3001 ): "Server started" => {
+    if (port === port3000 || port === port3001 ) {
+       console.log (`Server started on ${protocol}//sserver:${port}`); 
+    } else {
+        console.log("Invalid port");
+    }
+    
+    return "Server started"
+}
+
+startServer("https", 3001)
+
+const createAnimation = (
+    id: string | number, 
+    animName: string, 
+    limingFunc: "ease" | "ease-out" | "ease-in" = "ease", 
+    duration: number, 
+    iterCount: "infinite" | number 
+): void => { 
+    //const elem  = document.querySelector(`#${id}`) as HTMLElement;
+   // if ( elem ) {
+        console.log(`${animName} ${limingFunc} ${duration} ${iterCount}`)
+        //elem.style.animation  = `${animName} ${limingFunc} ${duration} ${iterCount}`
+    //}
+}
+
+createAnimation('id', 'fade', 'ease-in', 5, 'infinite')
