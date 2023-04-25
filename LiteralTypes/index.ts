92732a4e55aee8f43ba6e805ede6e83f1bb1323a
Evgeny <bril424@mail.ru>
2023-04-25 11:32:58 +0200

    objective literals and function  annotations

--- LiteralTypes/index.ts
+++ LiteralTypes/index.ts
@@ -2,20 +2,36 @@ let msg: "Hello" = "Hello"
 
 msg = "Hello"
 
-const port3000: number = 3000;
-const port3001: number = 3001
+const serverConfig: { protocol:"http" | "https"; port: 3000 | 3001   } = {
+    protocol: "https",
+    port: 3001
+}
 
-const startServer = (protocol: "http" | "https", port: 3000 | 3001 ): "Server started" => {
-    if (port === port3000 || port === port3001 ) {
-       console.log (`Server started on ${protocol}//sserver:${port}`); 
-    } else {
-        console.log("Invalid port");
-    }
-    
+const startServer: (protocol: "http" | "https", port: 3000 | 3001 ) => string = (
+    protocol: "http" | "https", 
+    port: 3000 | 3001, 
+): "Server started" => {
+    console.log (`Server started on ${protocol}//sserver:${port}`); 
+
     return "Server started"
 }
 
-startServer("https", 3001)
+startServer(serverConfig.protocol , serverConfig.port)
+
+// const port3000: number = 3000;
+// const port3001: number = 3001
+
+// const startServer = (protocol: "http" | "https", port: 3000 | 3001 ): "Server started" => {
+//     if (port === port3000 || port === port3001 ) {
+//        console.log (`Server started on ${protocol}//sserver:${port}`); 
+//     } else {
+//         console.log("Invalid port");
+//     }
+    
+//     return "Server started"
+// }
+
+// startServer("https", 3001)
 
 type AnimationTimingFunc = "ease" | "ease-out" | "ease-in";
 type AnimationID = string | number ;
