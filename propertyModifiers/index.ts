8d26638ebee310b0397c6bda8642cd3b99ec481f
Evgeny <bril424@mail.ru>
2023-05-10 11:43:57 +0200

    readonly (Property Modifiers) added

--- propertyModifiers/index.ts
+++ propertyModifiers/index.ts
@@ -1,9 +1,9 @@
 interface User {
-    login: string;
+    readonly login: string;
     password: string;
     age: number;
-    // addr?: string;
-    addr: string | undefined;
+    readonly addr?: string;
+    // addr: string | undefined;
     parents?: {
         mother?: string;
         father?: string;
@@ -17,6 +17,16 @@ const user: User = {
     addr: 'dkdfk'
 }
 
+const userFreeze: Readonly<User> = {
+    login: 'first!',
+    password: 'gwerty',
+    age: 50,
+    addr: 'dkdfk'
+}
+
+// userFreeze.age = 67;
+// userFreeze.password = "ioijojl"
+
 let  dbName: string;
 sendUserData(user, "sdfdsfje")
 console.log(dbName!)
@@ -25,3 +35,15 @@ function sendUserData ( obj: User, db?: string ): void  {
     dbName = '12345'
     console.log(obj.parents!.father?.toLowerCase(), db!.toLowerCase())
 }
+
+// class Animal {
+//     readonly name : string = "name"
+// }
+
+const basicPorts: ReadonlyArray<number> = [3000, 3001, 5555]
+// basicPorts [0] = 5
+// basicPorts.push(556)
+
+//const basicPorts: readonly [number, ...string[]] = [3000, '3001', '5555']
+// basicPorts [0] = 5
+// basicPorts.push(556)
