c0ba79d6462875b125746ef871927a66967f1312
Evgeny <bril424@mail.ru>
2023-07-03 10:43:32 +0200

    FunctionOverload added

---
+++ FunctionOverload/index.ts
@@ -0,0 +1,33 @@
+interface Square {
+    side: number;
+    area: number;
+}
+
+interface Rect {
+    a: number;
+    b: number;
+    area: number;
+}
+
+
+function calculateArea(side: number): Square;
+function calculateArea(a: number, b: number, c: number, d: number): Rect; 
+function calculateArea(a: number, b?: number, c?: number, d?: number): Square | Rect {
+    if (b) {
+        const rect: Rect = {
+            a, b,
+            area: a * b
+        }
+        return rect
+    } else {
+        const square: Square = {
+            side: a,
+            area: a * a
+        }
+
+        return square
+    }
+}
+
+calculateArea(1)
+calculateArea(1, 5, 5, 6)
