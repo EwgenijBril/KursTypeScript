77c539c3906640c8590fa3c3a97ec2bd20febb38
Evgeny <bril424@mail.ru>
2024-05-07 15:24:35 +0200

    added Indexed Access Types

--- KeyofTypeof/index.ts
+++ KeyofTypeof/index.ts
@@ -1,7 +1,22 @@
 interface ICompany {
     name: string;
     debts: number;
+    departments: Department[];
+    management: {
+        owner: string;
+    }
 }
+
+interface Department {
+    [key: string]: string;
+}
+let debts: "debts" = "debts";
+type CompanyDebtsType = ICompany[typeof debts];
+// type CompanyDebtsType = ICompany["debts"];
+type CompanyOwnerType = ICompany["management"]["owner"];
+type CompanyDepartmentsType = ICompany["departments"][number];
+type CompanyDepartmentsTypes = ICompany["departments"];
+type Test = ICompany[keyof ICompany];
 
 type CompanyKeys = keyof ICompany;
 const keys: CompanyKeys = "name";
@@ -14,19 +29,35 @@ function printDebs<T, K extends keyof T, S extends keyof T>(
     console.log(`Company ${company[name]}, debts: ${company[debts]}`)
 }
 
-const hh: ICompany = {
-    name: 'HH',
-    debts: 50000
-}
+// const hh: ICompany = {
+//     name: 'HH',
+//     debts: 50000
+// }
 
-printDebs(hh, "name", "debts");
+// printDebs(hh, "name", "debts");
 
-const google = {
-    name: "Google",
-    open: "true"
-}
+// const google: ICompany = {
+//     name: 'Google',
+//     debts: 50000,
+//     departments: {
+//         sales: "sales",
+//         developer: "dev"
+//     },
+//     management: {
+//         owner: "John",
+//     }
 
-printDebs(google, "name", "open");
+// }
 
-type GoogleKeys = keyof typeof google
-const keys2: GoogleKeys = "name";
+// printDebs(google, "name", "debts");
+
+
+// const google = {
+//     name: "Google",
+//     open: "true"
+// }
+
+// printDebs(google, "name", "open");
+
+// type GoogleKeys = keyof typeof google
+// const keys2: GoogleKeys = "name";
