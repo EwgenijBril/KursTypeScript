38ecbc0c96583eaf66c79dc57cd06c2ab4c29ce9
Evgeny <bril424@mail.ru>
2024-05-08 13:36:33 +0200

    ConditionalTypesAndInfer

---
+++ ConditionalTypesAndInfer/index.ts
@@ -0,0 +1,61 @@
+// Condition ? true :false
+// SomeType extends OtherType ? TrueType : FalseType
+
+const str: string = "Hello"
+type Example = "string" extends "Hello" ? string : number;
+
+type FromUserOrFromBase<T extends string | number> = T extends string
+? IDataFromUser 
+: IDataFromBase;
+
+interface User<T extends "created" | Date> {
+    created: T extends "created" ? "created" : Date
+}
+
+const user: User<"created"> = {
+    created: "created"
+}
+
+interface IDataFromUser {
+    weight: string;
+}
+
+interface IDataFromBase {
+    calories: number;
+}
+
+// function calculateDailyCalories(str: string): IDataFromUser;
+// function calculateDailyCalories(num: number): IDataFromBase;
+function calculateDailyCalories<T extends string | number >(
+    numOrStr: string | number
+): T extends string ? IDataFromUser : IDataFromBase {
+    if (typeof numOrStr === "string") {
+        const obj: IDataFromUser = {
+            weight: numOrStr,
+        };
+        return obj as FromUserOrFromBase<T>;
+    } else {
+        const obj: IDataFromBase = {
+            calories: numOrStr,
+        };
+        return obj as FromUserOrFromBase<T>;
+    }
+}
+
+type GetSringType<T extends "hello" | "world" | string> = T extends "hello"
+    ? "hello"
+    : T extends "world"
+    ? "world"
+    : string;
+
+type GetFirstType<T> = T extends Array<infer First> ? First : T;
+type Ex = GetFirstType<number[]>;
+
+type  ToArray<Type> = Type extends any ? Type[] : never;
+
+type ExArray = ToArray<Ex | string>
+
+
+
+
+
