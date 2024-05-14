12b87605f9cc9b7b7bc56e0aed5762d67e9f61c3
Evgeny <bril424@mail.ru>
2024-05-14 11:20:16 +0200

    base Classes

---
+++ BaseClasses/index.js
@@ -0,0 +1,9 @@
+var Box = /** @class */ (function () {
+    function Box(width) {
+        this.width = width;
+        this.height = 500;
+    }
+    return Box;
+}());
+var firstBox = new Box(250);
+console.log(firstBox);
---
+++ BaseClasses/index.ts
@@ -0,0 +1,21 @@
+class Box {
+    width: number;
+    height: number;
+
+    constructor(width: number) {
+        this.width = width;
+        this.height = 500;
+    }
+}
+
+const firstBox =new Box(250);
+console.log(firstBox)
+
+
+class User {
+    name: string;
+}
+
+const ivan = new User();
+ivan.name = "Ivan";
+console.log(ivan)
