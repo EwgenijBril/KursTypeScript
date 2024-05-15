33a451d472748cceb9f365af927180b7efac3bfd
Evgeny <bril424@mail.ru>
2024-05-15 11:42:14 +0200

    property visibility modifiers

---
+++ Modifiers/index.ts
@@ -0,0 +1,41 @@
+class Player {
+    private login: string;
+    private _password: string;
+    public server: string;
+    protected consent: boolean;
+
+    get password() {
+        return this._password;
+    }
+
+    set password(newPass: string) {
+        // Validation
+        this._password = newPass;
+    }
+ }
+
+ class CompetitivePlayer extends Player {
+    rank: number;
+    private isConsented() {
+        this.consent ? "Yes" : "No";
+    }
+ }
+
+const player = new CompetitivePlayer();
+// player.login = ";dfldkfj"
+player.password = "sdfjd";
+player
+
+// class User {
+//     public email: string;
+//     public name: string;
+
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+
+// class User {
+//     constructor(public email: string, public name:string) {}
+// }
